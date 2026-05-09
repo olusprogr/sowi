fa488605068a278b8a4b02901777b9b7cbb20a26
olusprogr <olus.main333@gmail.com>
2026-05-09 20:02:50 +0200

    wadwd

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'info', renderMode: RenderMode.Prerender },
+  { path: 'umfrage', renderMode: RenderMode.Prerender },
+  { path: 'impressum', renderMode: RenderMode.Prerender },
+  { path: 'admin/login', renderMode: RenderMode.Prerender },
+  { path: 'admin/register', renderMode: RenderMode.Prerender },
+  { path: 'stats', renderMode: RenderMode.Client },
+  { path: '**', renderMode: RenderMode.Client },
 ];
